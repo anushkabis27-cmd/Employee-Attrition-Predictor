d8e36f1f2696b9fdb8ccbb864bcfd534478714cf
anushkabis27-cmd <anushkabis27@gmail.com>
2026-05-05 19:19:49 +0530

    app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ st.markdown(f"""
 @st.cache_data
 def load_data():
     # Ensure the file exists in the GitHub root
-    data = pd.read_excel("Attrition_Final_Production_v6.xlsx")
+    data = pd.read_excel("Attrition_Final_Production_v5_Corrected.xlsx")
     data['EMPID'] = data['EMPID'].astype(str).str.zfill(6)
     return data
 
